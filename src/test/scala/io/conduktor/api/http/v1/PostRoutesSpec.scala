8b3c8c2c3081c1ab759910de093d0b2dfb3f4071
Matthieu Baechler <matthieu@apache.org>
2021-06-29 17:04:30 +0200

    Make e2e test work and share the app for the whole suite

--- src/test/scala/io/conduktor/api/http/v1/PostRoutesSpec.scala
+++ src/test/scala/io/conduktor/api/http/v1/PostRoutesSpec.scala
@@ -1,30 +1,50 @@
 package io.conduktor.api.http.v1
 
+import io.conduktor.api.BootstrapServer
+import io.conduktor.api.http.Server
+import sttp.capabilities
+import sttp.capabilities.zio.ZioStreams
 import sttp.client3._
 import sttp.client3.httpclient.zio.HttpClientZioBackend
 import sttp.model.StatusCode
-import zio.test.Assertion._
+import zio.test.Assertion.{containsString, equalTo, isRight}
+import zio.test.TestAspect.sequential
 import zio.test.environment.TestEnvironment
 import zio.test.{DefaultRunnableSpec, ZSpec, _}
-import io.conduktor.api.BootstrapServer
-import zio.magic.ZioProvideMagicOps
-import zio.ZIO
-import io.conduktor.api.http.Server
+import zio.{Has, Task, ZIO, ZLayer}
 
 object PostRoutesSpec extends DefaultRunnableSpec {
 
-  override def spec: ZSpec[TestEnvironment, Any] = suite("/v1/posts")(
+  type HttpClient = SttpBackend[Task, ZioStreams with capabilities.WebSockets]
+
+  val httpClient: ZLayer[Any, Throwable, Has[HttpClient]] = HttpClientZioBackend().toLayer
+
+  override def spec: ZSpec[TestEnvironment, Throwable] = suite("/v1/posts")(
     testM("POST / should return 200") {
-      val payload = """{
-                      | "title": "my test",
-                      | "content": "blabla"
-                      | }""".stripMargin
-      (for {
+      val payload =
+        """{
+          | "title": "my test",
+          | "content": "blabla"
+          | }""".stripMargin
+      for {
         server   <- ZIO.service[Server.Server]
-        client   <- HttpClientZioBackend()
+        client   <- ZIO.service[HttpClient]
         response <- basicRequest.body(payload).auth.bearer("Foo").post(uri"${server.baseUri}/v1/posts").send(client)
-      } yield assert(response.code)(equalTo(StatusCode.Ok))).provideCustomMagicLayer(BootstrapServer.localServer)
+      } yield assert(response.code)(equalTo(StatusCode.Ok))
+    },
+    testM("GET / should return a post") {
+      val payload =
+        """{
+          | "title": "my test",
+          | "content": "blabla"
+          | }""".stripMargin
+      for {
+        server   <- ZIO.service[Server.Server]
+        client   <- ZIO.service[HttpClient]
+        _        <- basicRequest.body(payload).auth.bearer("Foo").post(uri"${server.baseUri}/v1/posts").send(client)
+        response <- basicRequest.auth.bearer("Foo").get(uri"${server.baseUri}/v1/posts").send(client)
+      } yield assert(response.code)(equalTo(StatusCode.Ok)) && assert(response.body)(isRight(containsString("blabla")))
     }
-  )
+  ).provideSomeLayerShared((httpClient ++ BootstrapServer.localServer).orDie) @@ sequential
 
 }
