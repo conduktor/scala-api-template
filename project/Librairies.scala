98322b7f00a4b0b2bee0b3db5d8d1457d6996724
Stéphane Derosiaux <sderosiaux@conduktor.io>
2021-10-02 06:13:08 +0000

    Update zio-config, zio-config-magnolia, ... to 1.0.10

--- project/Librairies.scala
+++ project/Librairies.scala
@@ -3,7 +3,7 @@ import sbt._
 object Librairies {
 
   val zioVersion       = "1.0.10"
-  val zioConfigVersion = "1.0.6"
+  val zioConfigVersion = "1.0.10"
   val tapirVersion     = "0.18.3"
   val http4sVersion    = "0.22.0"
   val circeVersion     = "0.14.1"
