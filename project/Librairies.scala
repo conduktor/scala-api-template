ef2437f571e95c5ec06f8aa61e3381846276a6f7
Stéphane Derosiaux <sderosiaux@conduktor.io>
2022-05-31 00:15:46 +0000

    Update ws to 1.3.5

--- project/Librairies.scala
+++ project/Librairies.scala
@@ -44,7 +44,7 @@ object Librairies {
     "com.softwaremill.sttp.tapir"  %% "tapir-refined"           % tapirVersion,
     "com.softwaremill.sttp.tapir"  %% "tapir-newtype"           % tapirVersion,
     "com.softwaremill.sttp.tapir"  %% "tapir-json-circe"        % tapirVersion,
-    "com.softwaremill.sttp.shared" %% "ws"                      % "1.2.7" // resolve a conflict between client3 and tapir
+    "com.softwaremill.sttp.shared" %% "ws"                      % "1.3.5" // resolve a conflict between client3 and tapir
   )
 
   val client = Seq(
