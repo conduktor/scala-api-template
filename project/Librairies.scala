36c5cbcbeffc8df516958c5b6e266bdcf37e678b
Stéphane Derosiaux <sderosiaux@conduktor.io>
2022-08-18 13:04:58 +0200

    Update postgresql to 42.4.2 (#178)

--- project/Librairies.scala
+++ project/Librairies.scala
@@ -15,7 +15,7 @@ object Librairies {
   val refinedScalacheck = "eu.timepit"  %% "refined-scalacheck" % refinedVersion
   val flyway            = Seq(
     "org.flywaydb"   % "flyway-core" % "9.1.5",
-    "org.postgresql" % "postgresql"  % "42.4.1"
+    "org.postgresql" % "postgresql"  % "42.4.2"
   )
 
   val refined: Seq[ModuleID] = Seq(
