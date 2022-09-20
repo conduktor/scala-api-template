7d2c650263f160e8940e98defbedf41809cbd1fb
Stéphane Derosiaux <sderosiaux@conduktor.io>
2022-09-20 09:15:59 +0200

    Update zio, zio-test, zio-test-sbt to 1.0.17 (#205)

--- project/Librairies.scala
+++ project/Librairies.scala
@@ -2,7 +2,7 @@ import sbt._
 
 object Librairies {
 
-  val zioVersion       = "1.0.16"
+  val zioVersion       = "1.0.17"
   val zioConfigVersion = "2.0.9"
   val tapirVersion     = "1.0.4"
   val http4sVersion    = "0.23.12"
