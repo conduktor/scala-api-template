1dc3425362205a20cbcbe1ef718c594565df6b98
Stéphane Derosiaux <sderosiaux@conduktor.io>
2023-01-20 00:13:40 +0000

    Update flyway-core to 9.12.0

--- project/Librairies.scala
+++ project/Librairies.scala
@@ -14,7 +14,7 @@ object Librairies {
   val newtype           = "io.estatico" %% "newtype"            % "0.4.4"
   val refinedScalacheck = "eu.timepit"  %% "refined-scalacheck" % refinedVersion
   val flyway            = Seq(
-    "org.flywaydb"   % "flyway-core" % "9.11.0",
+    "org.flywaydb"   % "flyway-core" % "9.12.0",
     "org.postgresql" % "postgresql"  % "42.5.1"
   )
 
