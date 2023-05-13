6c1dc96641b51a81b40a5d5cd13205530f0bfd4b
Stéphane Derosiaux <sderosiaux@conduktor.io>
2023-05-13 00:11:31 +0000

    Update http4s-circe, http4s-dsl to 0.23.19

--- project/Librairies.scala
+++ project/Librairies.scala
@@ -5,7 +5,7 @@ object Librairies {
   val zioVersion       = "2.0.13"
   val zioConfigVersion = "2.0.9"
   val tapirVersion     = "1.0.4"
-  val http4sVersion    = "0.23.13"
+  val http4sVersion    = "0.23.19"
   val circeVersion     = "0.14.4"
   val refinedVersion   = "0.10.1"
   val sttpVersion      = "3.8.13"
