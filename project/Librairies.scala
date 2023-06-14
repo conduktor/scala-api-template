2170d453f5fe5b98481b2b3dcdc8eb9dbe8017c5
Stéphane Derosiaux <sderosiaux@conduktor.io>
2023-06-14 00:12:07 +0000

    Update logback-classic to 1.4.8

--- project/Librairies.scala
+++ project/Librairies.scala
@@ -66,7 +66,7 @@ object Librairies {
 
   val logging = Seq(
     "dev.zio"       %% "zio-logging-slf4j" % "2.1.12",
-    "ch.qos.logback" % "logback-classic"   % "1.4.7",
+    "ch.qos.logback" % "logback-classic"   % "1.4.8",
     "org.slf4j"      % "jul-to-slf4j"      % slf4jVersion,
     "org.slf4j"      % "log4j-over-slf4j"  % slf4jVersion,
     "org.slf4j"      % "jcl-over-slf4j"    % slf4jVersion,
